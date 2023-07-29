a10297489ed30ca2233f7863914946cec13b0d0d
藤田信行 <fujitanobuyuki@fujitanobuyukinoMacBook-Pro.local>
2023-07-29 14:30:40 +0900

    UI作りました

--- client/src/app/pages/chat/chat.component.ts
+++ client/src/app/pages/chat/chat.component.ts
@@ -12,4 +12,16 @@ export class ChatComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  year = 2022;
+  month = 7;
+  day = 30;
+  hours = 15;
+  minits = 30
+
+  time = `${this.year}/${this.month}/${this.day} ${this.hours}:${this.minits}`;
+  onSubmit(event:SubmitEvent){
+    event.preventDefault();
+    console.log("aiueo");
+  }
 }
+
